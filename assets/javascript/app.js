94c5721915612e7127fc689a89eb73b9fae3bd4e
roy769 <roy@mortgallery.com>
2017-07-24 10:35:28 -0700

    i can see the video box outline

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
 //set ready
 $( document ).ready(function() {
 
+$('#play-video').on('click',function(ev){
+  $)("#video") [0].src += "&autoplay=1";
+  ev.preventDefault();
+}
 //initialize vars
 var triva = {
   timer;
